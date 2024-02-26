cb84ede3de4d6d4780218a18fbabcae1ec42d66a
Rana <rana@gmail.com>
2024-02-26 12:56:18 +0530

    comit sheel page

--- MauiApp1SampleUpgrade/App.xaml.cs
+++ MauiApp1SampleUpgrade/App.xaml.cs
@@ -8,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AboutPage();
+		MainPage = new UpgradeXamarinForms.AppShell();
 	}
 }
--- MauiApp1SampleUpgrade/AppShell.xaml.cs
+++ MauiApp1SampleUpgrade/AppShell.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace MauiApp1SampleUpgrade;
+﻿using System;
+using System.Collections.Generic;
+using UpgradeXamarinForms.ViewModels;
+using UpgradeXamarinForms.Views;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui;
 
-public partial class AppShell : Shell
+namespace UpgradeXamarinForms
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public partial class AppShell : Microsoft.Maui.Controls.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
 }
